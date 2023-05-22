bf2750b34e1666bcc6f92809f6ecf94202a7daea
MustapheMS <moztaphem9@gmail.com>
2023-05-22 10:38:26 +0300

    Grade Done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello");
+
+const grade = 87;
+
+if (grade >= 90 && grade <= 100) {
+  console.log("You got an A grade");
+} else if (grade >= 75 && grade < 90) {
+  console.log("You got a B grade");
+} else if (grade >= 60 && grade < 75) {
+  console.log("You got a C grade");
+} else if (grade < 60) {
+  console.log("You got a FAILED grade");
+}
+
